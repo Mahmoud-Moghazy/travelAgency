37463f4a2699a08bc6fcbc77aa9a54cdca469e63
Mahmoud-Moghazy <ma.moghazy@gmail.com>
2025-05-01 18:33:04 +0300

    remove log user content from console

--- app/appwrite/auth.ts
+++ app/appwrite/auth.ts
@@ -84,7 +84,6 @@ export const logoutUser = async () => {
 export const getUser = async () => {
   try {
     const user = await account.get();
-    console.log("User:", user);
     if (!user) return redirect("/sign-in");
 
     const { documents } = await database.listDocuments(
