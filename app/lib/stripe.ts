6b250329df6cb1f135a35e7930d4ce9882316938
Mahmoud-Moghazy <ma.moghazy@gmail.com>
2025-05-01 18:07:36 +0300

    edit redirect to success page after payment

--- app/lib/stripe.ts
+++ app/lib/stripe.ts
@@ -29,7 +29,7 @@ export const createProduct = async (
     after_completion: {
       type: "redirect",
       redirect: {
-        url: `${process.env.VITE_BASE_URL}/travel/${tripId}/success`,
+        url: `https://travel-agency-opal-one.vercel.app/travel/${tripId}/success`,
       },
     },
   });
